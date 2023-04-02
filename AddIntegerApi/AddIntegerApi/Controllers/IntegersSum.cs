da9afde83ed0e6e8965f7bf7ee3d5a1e6cc624ab
Khang Nguyen <khang13102000@gmail.com>
2023-04-02 16:53:27 -0400

    First commit

---
+++ AddIntegerApi/AddIntegerApi/Controllers/IntegersSum.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddIntegerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegersSum : ControllerBase
+    {
+        [HttpGet("{num1}/{num2}")]
+        public IActionResult Sum(int num1, int num2)
+        {
+            return Ok(num1 + num2);
+            
+        }
+    }
+}
